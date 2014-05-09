9018a21a12ec9ebefff7613573084ffa4dbc2b45
sonali-bioc <sarora@fhcrc.org>
2014-05-08 20:48:02 -0700

    Create plot1.R

---
+++ R/plot1.R
@@ -0,0 +1,13 @@
+rm(list=ls())
+
+rawdata <- read.table(file, sep=";", header=TRUE)
+rawdata[,1]<- as.Date(as.character(rawdata[,1]), "%d/%m/%Y")
+data <- rawdata[which(rawdata$Date %in% as.Date(c("2007-02-02","2007-02-01"))),]
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time), 
+                            format="%Y-%m-%d %H:%M:%S")
+## plot1.R 
+png("plot1.png", width=480, height=480)
+hist(as.numeric(data[,3]), col="red", 
+     xlab="Global Active Power (kilowatts)", 
+     main ="Global Active Power")
+dev.off()
