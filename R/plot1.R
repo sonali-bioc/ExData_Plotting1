ac3a75dfc14546740d3d95b7ec607bf0f1a8a67d
sonali-bioc <sarora@fhcrc.org>
2014-05-08 20:50:45 -0700

    Update plot1.R

--- R/plot1.R
+++ R/plot1.R
@@ -1,5 +1,7 @@
 rm(list=ls())
 
+file <-"household_power_consumption.txt"
+
 rawdata <- read.table(file, sep=";", header=TRUE)
 rawdata[,1]<- as.Date(as.character(rawdata[,1]), "%d/%m/%Y")
 data <- rawdata[which(rawdata$Date %in% as.Date(c("2007-02-02","2007-02-01"))),]
